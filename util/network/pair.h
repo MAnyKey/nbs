cfb3a130028c26940561ec1ea0be8fd1a52fbe2b
Maxim Kolganov <manykey@nebius.com>
2024-02-05 20:36:54 +0100

    rm util

--- util/network/pair.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "init.h"
-
-int SocketPair(SOCKET socks[2], bool overlapped, bool cloexec = false);
-
-static inline int SocketPair(SOCKET socks[2]) {
-    return SocketPair(socks, false, false);
-}
